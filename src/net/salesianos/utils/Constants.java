a0f9a2c6a2097c07d07b4103b6128e77748d6d67
Isra-DVD <usr3849@salesianos-lacuesta.net>
2025-02-09 15:21:53 +0000

    feat(Constants): Implemented Constants class to store directories and port for conection

---
+++ src/net/salesianos/utils/Constants.java
@@ -0,0 +1,7 @@
+package net.salesianos.utils;
+
+public class Constants {
+    public static final int SERVER_PORT = 8082;
+    public static final String USER_FILES_DIRECTORY = "src\\net\\salesianos\\client\\client_files";
+    public static final String SERVER_FILES_DIRECTORY = "src\\net\\salesianos\\server\\server_files";
+}
